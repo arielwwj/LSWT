b6d9e0b2ec211b6e82f5a827d4e9adad5fc249bd
Weijia Wang <53646536+arielwwj@users.noreply.github.com>
2023-11-28 15:09:16 +0000

    ANN.m

--- ANN.m
+++ ANN.m
@@ -1,35 +1,33 @@
-%% run BP
+%% run ANN
 clc
 clear all
 
 i = 1;
-while i < 2261
+while i < Lake_num   % Lake_num is the number of studied lakes
     kk = 0;
-    while kk<20
-        name1 = ['I:\20230213修订后数据表格\20230921大修文件\BPNN\PRE_DATA\',num2str(i),'_P.csv'];
-        name2 = ['I:\20230213修订后数据表格\20230921大修文件\BPNN\PRE_DATA\',num2str(i),'_T.csv'];
-        chlaspectral = csvread(name1,0,0);
-        chla = csvread(name2,0,0);
+    while kk<20      % Provides 20 runs to find results that meet requirements
+        name1 = ['path'];  % X-data path
+        name2 = ['path'];  % Y-data path
+        X = csvread(name1,0,0);
+        Y = csvread(name2,0,0);
         
-        totalnum = length(chla);
+        totalnum = length(Y);
         num70 = round(totalnum*0.7);
-        % 随机产生训练集和测试集
-        temp = randperm(size(chlaspectral,1));
-        % 训练集
-        P_train = chlaspectral(temp(1:num70),:)';
-        T_train = chla(temp(1:num70),:)';
-        % 测试集——剩余样本
-        P_test = chlaspectral(temp(num70+1:end),:)';
-        T_test = chla(temp(num70+1:end),:)';
         
-        P_total = chlaspectral';
-        T_total = chla';
+        temp = randperm(size(X,1));
+        % train dataseet
+        P_train = X(temp(1:num70),:)';
+        T_train = Y(temp(1:num70),:)';
+        % test dataset
+        P_test = X(temp(num70+1:end),:)';
+        T_test = Y(temp(num70+1:end),:)';
+        
+        P_total = X';
+        T_total = Y';
         
         N_test = size(P_test,2);
         N_train = size(P_train,2);
         N_all = size(P_total,2);
-        
-        %数据归一化
         
         [p_total, ps_input] = mapminmax(P_total,0,1);
         p_test = mapminmax('apply',P_test,ps_input);
@@ -38,24 +36,23 @@ while i < 2261
         t_train = mapminmax('apply',T_train,ps_output);
         t_test = mapminmax('apply',T_test,ps_output);
         
-        %创建网络
+        %Create net
         
         net = newff(p_train,t_train,3);
         
-        % 设置训练参数
+        % set the training parameter
         net.trainParam.epochs = 1000;
         net.trainParam.goal = 1e-3;
         net.trainParam.lr = 0.01;
         
-        %训练网络
+        % train the net
         LSWT_model = train(net,p_train,t_train);
         
-        %仿真测试
+        %test
         t_sim_test_LSWT = sim(LSWT_model,p_test);
         t_sim_train_LSWT = sim(LSWT_model,p_train);
         t_sim_total_LSWT = sim(LSWT_model,p_total);
         
-        %数据反归一化??????
         T_sim_test_LSWT = mapminmax('reverse',t_sim_test_LSWT,ps_output);
         T_sim_train_LSWT = mapminmax('reverse',t_sim_train_LSWT,ps_output);
         T_sim_total_LSWT = mapminmax('reverse',t_sim_total_LSWT,ps_output);
@@ -64,7 +61,7 @@ while i < 2261
         error2 = abs(T_sim_train_LSWT - T_train)./T_train;
         error3 = abs(T_sim_total_LSWT - T_total)./T_total;
         
-        %????R^2
+        %R^2
         R2_1_LSWT = (N_test * sum(T_sim_test_LSWT .* T_test) - sum(T_sim_test_LSWT) * sum(T_test))^2 / ((N_test * sum((T_sim_test_LSWT).^2) - (sum(T_sim_test_LSWT))^2) * (N_test * sum((T_test).^2) - (sum(T_test))^2));
         R2_2_LSWT = (N_train * sum(T_sim_train_LSWT .* T_train) - sum(T_sim_train_LSWT) * sum(T_train))^2 / ((N_train * sum((T_sim_train_LSWT).^2) - (sum(T_sim_train_LSWT))^2) * (N_train * sum((T_train).^2) - (sum(T_train))^2));
         R2_3_LSWT = (N_all * sum(T_sim_total_LSWT .* T_total) - sum(T_sim_total_LSWT) * sum(T_total))^2 / ((N_all * sum((T_sim_total_LSWT).^2) - (sum(T_sim_total_LSWT))^2) * (N_all * sum((T_total).^2) - (sum(T_total))^2));
@@ -95,17 +92,14 @@ while i < 2261
             continue
             
         else
-            %namestring = [num2str(roundn(R2_2_LSWT,-2)),num2str(roundn(R2_1_LSWT,-2)),num2str(roundn(R2_3_LSWT,-2))];
-            Xlsxstring = ['I:\20230213修订后数据表格\20230921大修文件\BPNN\result\',num2str(i),'.xlsx'];
-            % modelstring = ['I:\20230213修订后数据表格\20230921大修文件\BPNN\',num2str(roundn(R2_2_LSWT,-2)),num2str(roundn(R2_1_LSWT,-2)),num2str(roundn(R2_3_LSWT,-2)),'.mat'];
+            Xlsxstring = ['path'];  %write path
             xlswrite(Xlsxstring,T_total','T_total')
             xlswrite(Xlsxstring,T_sim_total_LSWT','T_sim_total_LSWT')
         end
         i = i+1
         
     end
-    Xlsxstring = ['I:\20230213修订后数据表格\20230921大修文件\BPNN\result\',num2str(i),'.xlsx'];
-    % modelstring = ['I:\20230213修订后数据表格\20230921大修文件\BPNN\',num2str(roundn(R2_2_LSWT,-2)),num2str(roundn(R2_1_LSWT,-2)),num2str(roundn(R2_3_LSWT,-2)),'.mat'];
+    Xlsxstring = ['path'];
     xlswrite(Xlsxstring,T_total','T_total')
     xlswrite(Xlsxstring,T_sim_total_LSWT','T_sim_total_LSWT')
     i = i+1
